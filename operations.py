52b873572b83dcddc24c7ccace72f91a936a227a
devgupta-creates <devg12005@gmail.com>
2025-08-22 16:55:59 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,6 @@
+a=int(input('enter 1st number'))  #input
+b=int(input('enter 2nd number'))  #input
+print(f'addision= {a+b}')  
+print(f'substraction= {a-b}')
+print(f'division= {a/b}')
+print(f'multiplication= {a*b}')
